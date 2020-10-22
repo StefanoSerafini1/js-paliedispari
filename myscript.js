e89b5397efe7ec5c4839e779df7dee14ad3adf82
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-22 15:54:39 +0200

    agg stampa in html

--- myscript.js
+++ myscript.js
@@ -2,13 +2,17 @@
 
 var parola=prompt('Inserisci una parola');
 var parolainvertita=inversione(parola);
-
+document.getElementById('parola').innerHTML = 'La parola da te inserita è: ' + parola;
 console.log('Le due parole sono:  ' + parola + ' ' + parolainvertita);
+document.getElementById('invertita').innerHTML = 'La parola al contrario è: ' + parolainvertita;
 
 if(parola === parolainvertita){
   console.log('le due parole sono palindrome')
+  document.getElementById('palindroma').innerHTML = 'Le due parole sono palindrome';
+
 }else{
   console.console.log('le due parole non sono palindrome');
+  document.getElementById('palindroma').innerHTML = 'Le due parole NON sono palindrome';
 }
 
 function inversione(parola){
